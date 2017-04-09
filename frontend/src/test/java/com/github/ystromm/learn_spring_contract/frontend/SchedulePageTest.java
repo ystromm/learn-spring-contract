9add62b3e428a6b9e30e31dd0396922bfec0dc5c
Martin Carlsson <martin.carlsson@omegapoint.se>
2017-04-09 22:19:39 +0200

    Added group and version.

--- frontend/src/test/java/com/github/ystromm/learn_spring_contract/frontend/SchedulePageTest.java
+++ frontend/src/test/java/com/github/ystromm/learn_spring_contract/frontend/SchedulePageTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -22,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = {FrontEndMain.class})
+@AutoConfigureStubRunner(workOffline = true, ids = "com.github.ystrom.learn_spring_contract:backend:+:stubs:8081")
 public class SchedulePageTest {
 
     @Autowired
