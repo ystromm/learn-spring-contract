933390e2f11d9f15f21ba45802ace99dd2a0e0f2
Martin Carlsson <martin.carlsson@omegapoint.se>
2017-04-26 20:53:56 +0200

    Added java8 datetime mapping.

--- backend/src/main/java/com/github/ystromm/learn_spring_contract/backend/BackEndMain.java
+++ backend/src/main/java/com/github/ystromm/learn_spring_contract/backend/BackEndMain.java
@@ -1,11 +1,14 @@
 package com.github.ystromm.learn_spring_contract.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackEndMain {
     public static void main(String[] args) {
+        new ObjectMapper().registerModule(new JavaTimeModule());
         SpringApplication.run(BackEndMain.class, args);
     }
 }
