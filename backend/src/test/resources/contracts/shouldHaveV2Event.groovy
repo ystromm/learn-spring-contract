7adf0b9a7932f678fe274c9ad93aae0e48f0396c
Martin Carlsson <martin.carlsson@omegapoint.se>
2017-04-26 20:51:07 +0200

    Added java8 datetime mapping.

---
+++ backend/src/test/resources/contracts/shouldHaveV2Event.groovy
@@ -0,0 +1,23 @@
+package contracts
+
+org.springframework.cloud.contract.spec.Contract.make {
+    request {
+        method 'GET'
+        url '/event/1'
+        headers {
+        }
+    }
+    response {
+        status 200
+        body([
+                id      : 1,
+                speaker : 'Johan Malmliden',
+                description: 'State of the Nation',
+                location: 'Adelsö'
+        ])
+        headers {
+            contentType('application/json')
+        }
+    }
+
+}
--- frontend/src/main/java/com/github/ystromm/learn_spring_contract/frontend/FrontEndMain.java
+++ frontend/src/main/java/com/github/ystromm/learn_spring_contract/frontend/FrontEndMain.java
@@ -1,5 +1,8 @@
 package com.github.ystromm.learn_spring_contract.frontend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +12,9 @@ import org.springframework.context.annotation.ComponentScan;
 public class FrontEndMain {
 
     public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         SpringApplication.run(FrontEndMain.class, args);
     }
 }
